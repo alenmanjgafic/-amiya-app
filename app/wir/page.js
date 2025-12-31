bd0ef18888d004b83126903ec6d706785d7fc5f1
alenmanjgafic <alen.manjgafic@aman-gmbh.ch>
2025-12-31 13:00:26 +0100

    Update page.js

--- app/wir/page.js
+++ app/wir/page.js
@@ -301,12 +301,12 @@ export default function WirPage() {
         />
       )}
 
-      <style jsx global>{\`
+      <style jsx global>{`
         @keyframes spin {
           0% { transform: rotate(0deg); }
           100% { transform: rotate(360deg); }
         }
-      \`}</style>
+      `}</style>
     </div>
   );
 }
